b15677a9e0da629fe4acc6599fbfc9caac67db61
zamderax <zamderax@gmail.com>
2024-08-20 14:34:09 -0700

    adding readme

---
+++ README.md
@@ -0,0 +1,58 @@
+# Hybrid Logical Clock in Rust
+
+I built this crate to provide a hybrid logical clock in Rust so that I could learn about distributed systems.
+
+## Use
+
+You can create a new hybrid logical clock with the physical clock time set to the given value.
+
+```rs
+use hybrid_logical_clock::HybridLogicalClock;
+
+let hlc = HybridLogicalClock::new(100);
+```
+
+Sometimes you want to create a hybrid logical clock with both the physical and logical clock time set.
+
+```rs
+use hybrid_logical_clock::HybridLogicalClock;
+
+let hlc = HybridLogicalClock::new_with_both_physical_and_logical_clock_time(100, 100);
+```
+
+You can compare two hybrid logical clocks to see if they are causally related.
+
+```rs
+use hybrid_logical_clock::HybridLogicalClock;
+
+let hlc1 = HybridLogicalClock::new(100);
+let hlc2 = HybridLogicalClock::new(200);
+
+assert!(hlc1.is_concurrent(&hlc2));
+```
+
+## Why use a hybrid logical clock?
+
+Most distributed systems use lamport or logical clocks to order events. However, these clocks have several drawbacks:
+
+1. They do not provide causality tracking with ordering, meaning that two events can be causally related but not ordered. Ths is specifically important when building systems that have humans involved as humans seem to reach for physical time to determine ordering
+2. They do not provide a total order, meaning that two events can be concurrent but not ordered.
+3. They do not provide a way to detect conflicts, meaning that two events can be concurrent but not ordered.
+
+Hybrid Logical Clocks (HLCs) offer several advantages over traditional logical clocks or physical clocks alone:
+
+1. Causality tracking: HLCs combine the benefits of physical and logical clocks, allowing for accurate causality tracking between events in distributed systems.
+
+2. Better performance: Unlike vector clocks, HLCs have constant-size timestamps, leading to improved performance in large-scale distributed systems.
+
+3. Clock drift tolerance: HLCs can handle clock drift between nodes in a distributed system more gracefully than pure physical clocks.
+
+4. Monotonicity: HLCs ensure that timestamps always move forward, even if there are small backwards jumps in the physical clock.
+
+5. Fine-grained ordering: When multiple events occur at the same physical time, HLCs can still establish a total order using the logical component.
+
+6. Compatibility: HLCs can be used as a drop-in replacement for physical timestamps in many systems, providing additional causality information without major architectural changes.
+
+7. Conflict resolution: In distributed databases or collaborative editing systems, HLCs can help in detecting and resolving conflicts between concurrent updates.
+
+By using a Hybrid Logical Clock, you can achieve a more robust and accurate timekeeping mechanism in distributed systems, leading to better consistency and easier reasoning about event ordering.
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,102 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#![no_std]
+use core::cmp::Ordering;
+use core::ops::{Add, AddAssign};
+
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct HybridLogicalClock<P, L>
+where
+    P: Copy + Ord + Add<Output = P> + AddAssign,
+    L: Copy + Ord + Add<Output = L> + AddAssign,
+{
+    /**
+     * The physical clock time.
+     */
+    pub physical_clock_time: P,
+    /**
+     * The logical clock.
+     */
+    pub logical_clock: L,
+}
+
+impl<P, L> HybridLogicalClock<P, L>
+where
+    P: Copy + Ord + Add<Output = P> + AddAssign,
+    L: Copy + Ord + Add<Output = L> + AddAssign + Default + core::ops::Add<L, Output = L>,
+{
+    /**
+     * Create a new hybrid logical clock with the given physical clock time and logical clock time set to default.
+     * If you want to create a hybrid logical clock with both physical and logical clock time set, use the `new_with_both_physical_and_logical_clock_time` method instead.
+     * 
+     * @example
+     * ```
+     * let hlc = HybridLogicalClock::new(100);
+     * ```
+     */
+    pub fn new(physical_clock_time: P) -> Self {
+        HybridLogicalClock {
+            physical_clock_time,
+            logical_clock: L::default(),
+        }
+    }
+
+    /**
+     * Create a new hybrid logical clock with the given physical clock time and logical clock time.
+     * 
+     * @example
+     * ```
+     * let hlc = HybridLogicalClock::new_with_both_physical_and_logical_clock_time(100, 1);
+     * ```
+     */
+    pub fn new_with_both_physical_and_logical_clock_time(physical_clock_time: P, logical_clock_time: L) -> Self {
+        HybridLogicalClock {
+            physical_clock_time,
+            logical_clock: logical_clock_time,
+        }
+    }
+
+    /**
+     * Add the given number of logical clock ticks to the hybrid logical clock.
+     * 
+     * Use this method when you want to increment the logical clock without changing
+     * the physical clock time. This is useful in scenarios where multiple events
+     * occur at the same physical time, but you need to establish a causal order
+     * between them.
+     * 
+     * Note: This method uses wrapping addition to prevent overflow.
+     * 
+     * @example
+     * ```
+     * let mut hlc = HybridLogicalClock::new(100);
+     * hlc.add_logical_clock_ticks(1); // Increment logical clock by 1
+     * hlc.add_logical_clock_ticks(5); // Increment logical clock by 5
+     * ```
+     */
+    pub fn add_logical_clock_ticks(&mut self, ticks: L) {
+        self.logical_clock = self.logical_clock + ticks;    
+    }
+}
+
+impl<P, L> PartialOrd for HybridLogicalClock<P, L>
+where
+    P: Copy + Ord + Add<Output = P> + AddAssign,
+    L: Copy + Ord + Add<Output = L> + AddAssign,
+{
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<P, L> Ord for HybridLogicalClock<P, L>
+where
+    P: Copy + Ord + Add<Output = P> + AddAssign,
+    L: Copy + Ord + Add<Output = L> + AddAssign,
+{
+    fn cmp(&self, other: &Self) -> Ordering {
+        match self.physical_clock_time.cmp(&other.physical_clock_time) {
+            Ordering::Equal => self.logical_clock.cmp(&other.logical_clock),
+            other => other,
+        }
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +104,36 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_new() {
+        let hlc = HybridLogicalClock::<u64, u32>::new(100);
+        assert_eq!(hlc.physical_clock_time, 100);
+        assert_eq!(hlc.logical_clock, 0);
+    }
+
+    #[test]
+    fn test_new_with_both_physical_and_logical_clock_time() {
+        let hlc = HybridLogicalClock::<u64, u32>::new_with_both_physical_and_logical_clock_time(100, 1);
+        assert_eq!(hlc.physical_clock_time, 100);
+        assert_eq!(hlc.logical_clock, 1);
+    }
+
+    #[test]
+    fn test_add_logical_clock_ticks() {
+        // at this point, the logical clock is 0
+        let mut hlc = HybridLogicalClock::<u64, u32>::new(100);
+        
+        hlc.add_logical_clock_ticks(1);
+        assert_eq!(hlc.logical_clock, 1);
+
+        let mut hlc_with_both_physical_and_logical_clock_time = HybridLogicalClock::new_with_both_physical_and_logical_clock_time(100, 20); 
+        hlc_with_both_physical_and_logical_clock_time.add_logical_clock_ticks(23);
+        assert_eq!(hlc_with_both_physical_and_logical_clock_time.logical_clock, 43);
+    }
+
+    #[test]
+    fn test_cmp() {
+        let hlc1 = HybridLogicalClock::new_with_both_physical_and_logical_clock_time(100, 20);
+        let hlc2 = HybridLogicalClock::new_with_both_physical_and_logical_clock_time(100, 20);
+        assert_eq!(hlc1, hlc2);
     }
 }
